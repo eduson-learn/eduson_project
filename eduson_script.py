9b9b4d7b97e814c53d1e4a8bc4b1125872591f26
teywill <andrey.belenkiy.il@gmail.com>
2022-12-08 13:11:21 +0200

    Initial commit by Eduson

---
+++ eduson_script.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    if True:
+        a = 2
+        print('Hello, world!')
+
+    print('I am inside main!')
---
+++ whitespaces_pep.py
@@ -0,0 +1,12 @@
+# между скобкой и значением
+print('Hello, world!')
+
+# между запятой и закрывающей скобкой
+books = ('War and Peace', 'Anna Karenina',)
+
+# перед , : ;
+if True:
+    print('Hello', 'world!')
+
+# между именем функции и скобками с аргументом
+print('Hello, world!')
